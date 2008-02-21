35437e87842a84092241c68bddfbe7cd020a222d
Stefan Hundhammer <sh@suse.de>
2008-02-21 12:51:50 +0000

    pkg split
    svn path=/trunk/libyui/; revision=44741

---
+++ examples/HelloWorld.cc
@@ -0,0 +1,24 @@
+// Trivial libyui example.
+//
+// Compile with:
+//
+//     g++ -I/usr/include/YaST2/yui -lyui HelloWorld.cc -o HelloWorld
+
+#include "YUI.h"
+#include "YWidgetFactory.h"
+#include "YDialog.h"
+#include "YLayoutBox.h"
+#include "YEvent.h"
+
+int main( int argc, char **argv )
+{
+    YDialog    * dialog = YUI::widgetFactory()->createPopupDialog();
+    YLayoutBox * vbox   = YUI::widgetFactory()->createVBox( dialog );
+    YUI::widgetFactory()->createLabel     ( vbox, "Hello, World!" );
+    YUI::widgetFactory()->createPushButton( vbox, "&OK" );
+    
+    YEvent * event = dialog->waitForEvent();
+    delete event;
+
+    dialog->destroy();
+}
