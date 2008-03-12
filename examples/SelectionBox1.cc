89474419570f76b2a0fdf3d16cc882604bc10135
Stefan Hundhammer <sh@suse.de>
2008-03-12 18:07:45 +0000

    simple and advanced example
    svn path=/trunk/libyui/; revision=45405

--- examples/SelectionBox1.cc
+++ examples/SelectionBox1.cc
@@ -1,4 +1,4 @@
-// SelectionBox example.
+// Simple SelectionBox example.
 //
 // Compile with:
 //
@@ -12,39 +12,10 @@
 #include "YDialog.h"
 #include "YLayoutBox.h"
 #include "YSelectionBox.h"
-#include "YCheckBox.h"
 #include "YLabel.h"
 #include "YPushButton.h"
 #include "YAlignment.h"
 #include "YEvent.h"
-
-
-YItemCollection pizzaItems()
-{
-    YItemCollection items;
-    items.push_back( new YItem( "Pizza Margherita" 	 ) );
-    items.push_back( new YItem( "Pizza Capricciosa"	 ) );
-    items.push_back( new YItem( "Pizza Funghi"		 ) );
-    items.push_back( new YItem( "Pizza Prosciutto"	 ) );
-    items.push_back( new YItem( "Pizza Quattro Stagioni" ) );
-    items.push_back( new YItem( "Calzone"		 ) );
-
-    return items;
-}
-
-
-YItemCollection pastaItems()
-{
-    YItemCollection items;
-    items.push_back( new YItem( "Spaghetti Napoli" 	 ) );
-    items.push_back( new YItem( "Spaghetti Bolognese" 	 ) );
-    items.push_back( new YItem( "Fusili Emiliana" 	 ) );
-    items.push_back( new YItem( "Penne Calabrese" 	 ) );
-    items.push_back( new YItem( "Penne Al Forno" 	 ) );
-    items.push_back( new YItem( "Lasagne"	 	 ) );
-
-    return items;
-}
 
 
 
@@ -57,36 +28,20 @@ int main( int argc, char **argv )
     YDialog    * dialog  = YUI::widgetFactory()->createPopupDialog();
     YLayoutBox * vbox    = YUI::widgetFactory()->createVBox( dialog );
 
-    // Specify larger size for the SelectionBox: It can scroll, so its size
-    // depends on other widgets in the layout.
-    YAlignment * minSize = YUI::widgetFactory()->createMinSize( vbox, 40, 8 ); // minWidth, minHeight
+    YSelectionBox * selBox = YUI::widgetFactory()->createSelectionBox( vbox, "&Menu" );
 
-    //
-    // This is what this example all about.
-    // The other widgets are here just to allow some experimenting.
-    //
+    YItemCollection items;
+    items.push_back( new YItem( "Pizza Margherita" 	 ) );
+    items.push_back( new YItem( "Pizza Capricciosa"	 ) );
+    items.push_back( new YItem( "Pizza Funghi"		 ) );
+    items.push_back( new YItem( "Pizza Prosciutto"	 ) );
+    items.push_back( new YItem( "Pizza Quattro Stagioni" ) );
+    items.push_back( new YItem( "Calzone"		 ) );
+    selBox->addItems( items ); // This is more efficient than repeatedly calling selBox->addItem()
 
-    YSelectionBox * selBox = YUI::widgetFactory()->createSelectionBox( minSize, "&Menu" );
-    selBox->addItems( pizzaItems() );
-
-    YLayoutBox    * buttonBox	= YUI::widgetFactory()->createHBox( vbox );
-    YPushButton   * pastaButton = YUI::widgetFactory()->createPushButton( buttonBox, "&Pasta" );
-    YPushButton   * pizzaButton = YUI::widgetFactory()->createPushButton( buttonBox, "Pi&zza" );
-
-    YUI::widgetFactory()->createVSpacing( vbox, 0.5 );
-
-    YAlignment * leftAlignment = YUI::widgetFactory()->createLeft( vbox );
-    YCheckBox * notifyCheckBox = YUI::widgetFactory()->createCheckBox( leftAlignment, "&Notify Mode" );
-    notifyCheckBox->setNotify();
-
-    leftAlignment = YUI::widgetFactory()->createLeft( vbox );
-    YCheckBox * immediateCheckBox = YUI::widgetFactory()->createCheckBox( leftAlignment, "&Immediate Mode" );
-    immediateCheckBox->setNotify();
-
-    YUI::widgetFactory()->createVSpacing( vbox, 0.3 );
 
     YLayoutBox * hbox = YUI::widgetFactory()->createHBox( vbox );
-    YLabel * valueField  = YUI::widgetFactory()->createOutputField( hbox, "???" );
+    YLabel * valueField  = YUI::widgetFactory()->createOutputField( hbox, "<SelectionBox value unknown>" );
     valueField->setStretchable( YD_HORIZ, true ); // allow stretching over entire dialog width
 
     YPushButton * valueButton = YUI::widgetFactory()->createPushButton( hbox, "&Value" );
@@ -118,44 +73,9 @@ int main( int argc, char **argv )
 
 		if ( widgetEvent->widget() == closeButton )
 		    break; // leave event loop
-		else if ( widgetEvent->widget() == pastaButton )
-		{
-		    selBox->deleteAllItems();
-		    selBox->addItems( pastaItems() );
-		}
-		else if ( widgetEvent->widget() == pizzaButton )
-		{
-		    selBox->deleteAllItems();
-		    selBox->addItems( pizzaItems() );
-		}
-		else if ( widgetEvent->widget() == notifyCheckBox )
-		{
-		    bool notify = notifyCheckBox->isChecked();
-
-		    if ( ! notify )
-		    {
-			// immediateMode implicitly includes notify, so set
-			// immediateMode off if the user wants to set notify off
-
-			selBox->setImmediateMode( false );
-			immediateCheckBox->setChecked( false );
-		    }
-
-		    selBox->setNotify( notify );
-		}
-		else if ( widgetEvent->widget() == immediateCheckBox )
-		{
-		    bool immediate = immediateCheckBox->isChecked();
-		    selBox->setImmediateMode( immediate );
-
-		    // immediateMode implicitly includes notify;
-		    // reflect this in the notify check box
-
-		    if ( immediate )
-			notifyCheckBox->setChecked( true );
-		}
-		else if ( widgetEvent->widget() == selBox ||
-			  widgetEvent->widget() == valueButton )
+		
+		if ( widgetEvent->widget() == valueButton ||
+		     widgetEvent->widget() == selBox ) // selBox will only send events with setNotify()
 		{
 		    YItem * item = selBox->selectedItem();
 
