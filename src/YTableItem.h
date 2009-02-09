9f8ac9f0c7a5d42e9f03e4f27c75ee8a2c91aba2
Stefan Hundhammer <sh@suse.de>
2009-02-09 10:55:45 +0000

    - Added virtual destructor to YTableCell for better NCFileSelection support -> libyui API change - Require latest libyui in packages using libyui
    svn path=/trunk/libyui/; revision=55335

--- src/YTableItem.h
+++ src/YTableItem.h
@@ -234,8 +234,13 @@ public:
 	, _column ( column )
         {}
 
-    // No virtual destructor. This saves a vtable for this class since there
-    // are no other virtual methods either.
+    /**
+     * Destructor. Not strictly needed inside this class, but useful for
+     * derived classes. Since this is the only virtual method of this class,
+     * the cost of this is a vtable for this class and a pointer to the vtable
+     * in each instance.
+     **/
+    virtual ~YTableCell() {}
 
     /**
      * Return this cells's label. This is what the user sees in a dialog, so
