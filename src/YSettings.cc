2f5a16d85c832b6d5e74552382a260db239540e8
Björn Esser <bjoern.esser@gmail.com>
2012-06-07 10:11:52 +0200

    introduce YSettings With this new function a program's preferred subdir can be set once. It is recommended to do so in your programm at first (before init of UI). Once you set this, the subdir's value can't be altered. It will get automagically included through YUI.h.
    use: YSettings::access()->setProgSubDir( string subDir );

---
+++ src/YSettings.cc
@@ -0,0 +1,114 @@
+/*************************************************************************************************************
+
+ Copyright (C) 2000 - 2010 Novell, Inc.   All Rights Reserved.
+
+ This program is free software; you can redistribute it and/or modify it under the terms of the GNU General
+ Public License as published by the Free Software Foundation; either version 2 of the License, or (at your
+ option) any later version.
+
+ This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+ implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ for more details.
+
+ You should have received a copy of the GNU General Public License along with this program; if not, write to
+ the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+*************************************************************************************************************/
+
+
+
+ /////////////////////////////////////////////////////////////////////////////////////////////////////////////
+ /////////////////////////////////////////////////////////////////////////////////////////////////////////////
+ ////                                                                                                     ////
+ ////                                                                                                     ////
+ ////                                                                                                     ////
+ ////   __/\\\\\\_____________/\\\__________/\\\________/\\\___/\\\________/\\\___/\\\\\\\\\\\_           ////
+ ////    _\////\\\____________\/\\\_________\///\\\____/\\\/___\/\\\_______\/\\\__\/////\\\///__          ////
+ ////     ____\/\\\______/\\\__\/\\\___________\///\\\/\\\/_____\/\\\_______\/\\\______\/\\\_____         ////
+ ////      ____\/\\\_____\///___\/\\\_____________\///\\\/_______\/\\\_______\/\\\______\/\\\_____        ////
+ ////       ____\/\\\______/\\\__\/\\\\\\\\\_________\/\\\________\/\\\_______\/\\\______\/\\\_____       ////
+ ////        ____\/\\\_____\/\\\__\/\\\////\\\________\/\\\________\/\\\_______\/\\\______\/\\\_____      ////
+ ////         ____\/\\\_____\/\\\__\/\\\__\/\\\________\/\\\________\//\\\______/\\\_______\/\\\_____     ////
+ ////          __/\\\\\\\\\__\/\\\__\/\\\\\\\\\_________\/\\\_________\///\\\\\\\\\/_____/\\\\\\\\\\\_    ////
+ ////           _\/////////___\///___\/////////__________\///____________\/////////______\///////////__   ////
+ ////                                                                                                     ////
+ ////                                                                                                     ////
+ ////                 widget abstraction library providing Qt, GTK and ncurses frontends                  ////
+ ////                                                                                                     ////
+ ////                                   3 UIs for the price of one code                                   ////
+ ////                                                                                                     ////
+ ////                                       ***  core library  ***                                        ////
+ ////                                                                                                     ////
+ ////                                                                                                     ////
+ ////                                                                                                     ////
+ ////                                                                              (C) SUSE Linux GmbH    ////
+ ////                                                                                                     ////
+ ////                                                              libYUI-AsciiArt (C) 2012 Björn Esser   ////
+ ////                                                                                                     ////
+ /////////////////////////////////////////////////////////////////////////////////////////////////////////////
+ /////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+/*-/
+
+  File:		YSettings.cc
+
+  Author:	Björn Esser <bjoern.esser@gmail.com>
+
+/-*/
+
+#include "YSettings.h"
+
+#define YUILogComponent "ui"
+#include "YUILog.h"
+
+using namespace std;
+
+YSettings * YSettings::_instance = 0;
+string YSettings::progSubDir = "";
+
+YSettings * YSettings::access ()
+{
+  if ( ! _instance )
+    _instance = new YSettings ();
+  return _instance;
+}
+
+YSettings::YSettings()
+{
+  progSubDir = "";
+}
+
+YSettings::~YSettings ()
+{
+  deleteSettings ();
+}
+
+void YSettings::setProgSubDir( string directory )
+{
+  if ( progSubDir.compare ( "" ) == 0 )
+  {
+    progSubDir = directory;
+    yuiMilestone () << "Set progSubDir to \"" << directory << "\"" << endl;
+    yuiMilestone () << "progSubDir is now locked." << endl;
+  }
+  else
+  {
+    yuiMilestone () << "Can't set progSubDir to \"" << directory << "\"" << endl;
+    yuiMilestone () << "It is locked to: \"" << progSubDir << "\"" << endl;
+  }
+}
+
+string YSettings::getProgSubDir ()
+{
+  return progSubDir;
+}
+
+void YSettings::deleteSettings ()
+{
+  if( NULL != _instance )
+  {
+    delete _instance;
+    _instance = NULL;
+  }
+}
