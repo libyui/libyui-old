3d8d03311a5188f50c837fef065b735d86f0e49d
Björn Esser <bjoern.esser@gmail.com>
2012-06-13 09:23:04 +0200

    update of buildtools

--- RPMINFO.cmake
+++ RPMINFO.cmake
@@ -2,24 +2,20 @@
 #####
 ##### THIS IS THE INFO REQUIRED FOR SPEC-FILE GENERATION
 
-SET( SPEC_URL			"http://github.com/libyui/" )							# the URL of the project
+SET( SPEC_URL			"http://github.com/libyui/" )
+SET( SPEC_SUMMARY		"GUI-abstraction library" )
 
-SET( SPEC_SUMMARY		"GUI-abstraction library" )							# some brief summary
-
-SET( SPEC_DESCRIPTION		"This is the user interface engine that provides the abstraction from\n" )	# the description to be used, end each line with "\n" for line-breaks
+SET( SPEC_DESCRIPTION		"This is the user interface engine that provides the abstraction from\n" )
 SET( SPEC_DESCRIPTION		"${SPEC_DESCRIPTION}graphical user interfaces (Qt, Gtk) and text based user interfaces\n" )
 SET( SPEC_DESCRIPTION		"${SPEC_DESCRIPTION}(ncurses).\n\n" )
 SET( SPEC_DESCRIPTION		"${SPEC_DESCRIPTION}Originally developed for YaST, it can now be used independently of\n" )
 SET( SPEC_DESCRIPTION		"${SPEC_DESCRIPTION}YaST for generic (C++) applications. This package has very few\n" )
 SET( SPEC_DESCRIPTION		"${SPEC_DESCRIPTION}dependencies." )
 
-SET( SPEC_BuildRequires		"boost-devel" "cmake >= 2.8" "gcc-c++" )					# the BuildRequires every single pkg wrapped with "" and speparated with spaces
-
-SET( SPEC_Provides		"libyui = %{version}" "yast2-libyui = 2.21.5" )					# the Provides every single pkg wrapped with "" and speparated with spaces
-
-SET( SPEC_Obsoletes		"yast2-libyui < 2.21.5" )							# the Obsoletes every single pkg wrapped with "" and speparated with spaces
-
-SET( SPEC_DEVEL_Requires	"@PROJECTNAME@@@varpfx@_SONAME_MAJOR@ = %{version}" )				# the Requires for the -devel pkg every single pkg wrapped with "" and speparated with spaces
+SET( SPEC_BuildRequires		"boost-devel" "cmake >= 2.8" "gcc-c++" )
+SET( SPEC_Provides		"libyui = %{version}" "yast2-libyui = 2.21.5" )
+SET( SPEC_Obsoletes		"yast2-libyui < 2.21.5" )
+SET( SPEC_DEVEL_Requires	"@PROJECTNAME@@@varpfx@_SONAME_MAJOR@ = %{version}" )
 SET( SPEC_DEVEL_Requires	${SPEC_DEVEL_Requires} "glibc-devel" "libstdc++-devel" "boost-devel" )
-
-SET( SPEC_DEVEL_Provides	"pkgconfig(@PROJECTNAME@) = %{version}" )					# the Provides for the -devel pkg
+SET( SPEC_DEVEL_Provides	"pkgconfig(@PROJECTNAME@) = %{version}" )
+SET( SPEC_DEVEL_DOCS            "%doc %{_docdir}/@PROJECTNAME@" )
--- macros.cmake
+++ macros.cmake
@@ -104,8 +104,8 @@ MACRO( SET_ENVIRONMENT )	# setup the environment vars
 
   SET( CMAKE_PREFIX_PATH "${CMAKE_PREFIX_PATH}" "${PREFIX}" )
   SET( CMAKE_INSTALL_PREFIX "${PREFIX}" )
-  SET( CMAKE_LIBRARY_PATH ${CMAKE_LIBRARY_PATH} "${PREFIX}/${LIB_DIR}" )
-  SET( CMAKE_INCLUDE_PATH "${CMAKE_INCLUDE_PATH}" "${PREFIX}/${INCLUDE_DIR}" )
+  SET( CMAKE_LIBRARY_PATH ${CMAKE_LIBRARY_PATH} "${PREFIX}/${LIB_DIR}" "${PREFIX}/${LIB_DIR}/${BASELIB}" )
+  SET( CMAKE_INCLUDE_PATH "${CMAKE_INCLUDE_PATH}" "${PREFIX}/${INCLUDE_DIR}" "${EXTRA_INCLUDES}" )
   SET( INSTALL_CMAKE_DIR ${LIB_DIR}/cmake/${PROJECTNAME} CACHE PATH "Installation directory for CMake files" )
   SET( INSTALL_PKGCONFIG_DIR ${LIB_DIR}/pkgconfig CACHE PATH "Installation directory for pkgconfig files" )
 
@@ -155,12 +155,12 @@ MACRO( FIND_LIB_DEPENDENCIES )	# try to find all libs from ${LIB_DEPS}
     FIND_PACKAGE( ${p} REQUIRED )
     SET( CMAKE_INCLUDE_PATH ${${p}_INCLUDE_DIR} ${CMAKE_INCLUDE_PATH} )
     SET( CMAKE_INCLUDE_PATH ${${p}_INCLUDE_DIRS} ${CMAKE_INCLUDE_PATH} )
-    SET( ${LIB_LINKER} ${${p}_LIBRARIES} ${LIB_LINKER} )
+    SET( LIB_LINKER ${${p}_LIBRARIES} ${LIB_LINKER} )
   ENDFOREACH()
 
   IF( PLUGINNAME )
     FOREACH( p "" _MAJOR _MINOR _PATCH )
-      SET( SONAME${p} "${Lib${BASENAME}_SONAME${p}}")
+      SET( SONAME${p} "${Lib${BASELIB}_SONAME${p}}")
     ENDFOREACH()
   ENDIF( PLUGINNAME )
 
@@ -322,14 +322,21 @@ MACRO( GEN_FILES )		# generate files from templates
     @ONLY
   )
 
-  CONFIGURE_FILE(
-    "${PROJECT_SOURCE_DIR}/resource/config.h.in"
-    "${PROJECT_BINARY_DIR}/src/${PROJECTNAME_UC}_config.h"
-    @ONLY
-  )
+  IF( NOT PLUGINNAME )
+    CONFIGURE_FILE(
+      "${PROJECT_SOURCE_DIR}/resource/config.h.in"
+      "${PROJECT_BINARY_DIR}/src/${PROJECTNAME_UC}_config.h"
+      @ONLY
+    )
 
-  FOREACH( p "${PROJECTNAME_UC}Config.cmake" "${PROJECTNAME_UC}ConfigVersion.cmake" "${PROJECTNAME}.pc"
-           "src/${PROJECTNAME_UC}_config.h" )
+    CONFIGURE_FILE(
+      "${PROJECT_BINARY_DIR}/src/${PROJECTNAME_UC}_config.h"
+      "${PROJECT_BINARY_DIR}/src/${PROJECTNAME_UC}_config.h"
+      @ONLY
+    )
+  ENDIF( NOT PLUGINNAME )
+
+  FOREACH( p "${PROJECTNAME_UC}Config.cmake" "${PROJECTNAME_UC}ConfigVersion.cmake" "${PROJECTNAME}.pc" )
     CONFIGURE_FILE(
       "${PROJECT_BINARY_DIR}/${p}"
       "${PROJECT_BINARY_DIR}/${p}"
