bb5d50b04c83646ad0b40baeb70026a0993c16ce
Björn Esser <bjoern.esser@gmail.com>
2012-06-14 14:16:43 +0200

    update copyright-headers to LGPL 2.1/3.0

--- src/FSize.h
+++ src/FSize.h
@@ -1,25 +1,16 @@
 /*
-  |****************************************************************************
-  |
-  | Copyright (c) 2000 - 2012 Novell, Inc.
-  | All Rights Reserved.
-  |
-  | This program is free software; you can redistribute it and/or
-  | modify it under the terms of version 2 of the GNU General Public License as
-  | published by the Free Software Foundation.
-  |
-  | This program is distributed in the hope that it will be useful,
-  | but WITHOUT ANY WARRANTY; without even the implied warranty of
-  | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.   See the
-  | GNU General Public License for more details.
-  |
-  | You should have received a copy of the GNU General Public License
-  | along with this program; if not, contact Novell, Inc.
-  |
-  | To contact Novell about this file by physical or electronic mail,
-  | you may find current contact information at www.novell.com
-  |
-  |****************************************************************************
+  Copyright (C) 1970-2012 Novell, Inc
+  This library is free software; you can redistribute it and/or modify
+  it under the terms of the GNU Lesser General Public License as
+  published by the Free Software Foundation; either version 2.1 of the
+  License, or (at your option) version 3.0 of the License. This library
+  is distributed in the hope that it will be useful, but WITHOUT ANY
+  WARRANTY; without even the implied warranty of MERCHANTABILITY or
+  FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+  License for more details. You should have received a copy of the GNU
+  Lesser General Public License along with this library; if not, write
+  to the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+  Floor, Boston, MA 02110-1301 USA
 */
 
 
