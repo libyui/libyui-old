bc2fb0a76aae5b8e57bb604939cd715e9c6ec5e1
Thomas Goettlicher <tgoettlicher@suse.de>
2012-08-03 14:57:37 +0200

    fixed license

--- src/YTable.cc
+++ src/YTable.cc
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 1970-2012 Novell, Inc
+  Copyright (C) 2000-2012 Novell, Inc
   This library is free software; you can redistribute it and/or modify
   it under the terms of the GNU Lesser General Public License as
   published by the Free Software Foundation; either version 2.1 of the
