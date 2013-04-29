db233d01f5c4f905d9acc13a0a7ef043bbfd55bf
Thomas Goettlicher <tgoettlicher@suse.de>
2013-04-29 08:59:47 +0200

    adapted version

--- VERSION.cmake
+++ VERSION.cmake
@@ -5,7 +5,7 @@ SET( VERSION "${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_PATCH}${GIT_SHA1_VERSI
 
 ##### This is need for the libyui core, ONLY.
 ##### These will be overridden from exports in LibyuiConfig.cmake
-SET( SONAME_MAJOR "4" )
-SET( SONAME_MINOR "1" )
+SET( SONAME_MAJOR "5" )
+SET( SONAME_MINOR "0" )
 SET( SONAME_PATCH "0" )
 SET( SONAME "${SONAME_MAJOR}.${SONAME_MINOR}.${SONAME_PATCH}" )
