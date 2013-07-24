25899fbd0566f9d5b3240731c87e4b95c06edfb8
Thomas Goettlicher <tgoettlicher@suse.de>
2013-07-24 05:58:29 -0700

    Fixed bnc#752278: ComboBox does not show active item when item with empty label is present

--- src/YComboBox.cc
+++ src/YComboBox.cc
@@ -137,21 +137,15 @@ YComboBox::selectedItem()
 {
     std::string currentText = text();
 
-    if ( ! currentText.empty() )
+    // Try to find an item with this text
+    YItem * item = findItem( currentText );
+
+    if ( item )
     {
-	// Try to find an item with this text
-
-	YItem * item = findItem( currentText );
-
-	if ( item )
-	{
-	    // Make sure exactly this item is selected (and no other)
-
-	    YSelectionWidget::deselectAllItems();
-	    item->setSelected( true );
-
-	    return item;
-	}
+        // Make sure exactly this item is selected (and no other)
+        YSelectionWidget::deselectAllItems();
+        item->setSelected( true );
+        return item;
     }
     else
     {
