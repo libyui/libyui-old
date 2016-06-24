785cb5a5cc95d8dff6bc642532e2565810f98752
Ladislav Slezák <lslezak@suse.cz>
2016-06-24 16:05:09 +0200

    Allow editing the properties

--- src/YDialogSpy.cc
+++ src/YDialogSpy.cc
@@ -38,6 +38,9 @@
 #include <YAlignment.h>
 #include <YButtonBox.h>
 #include <YPushButton.h>
+#include <YMenuButton.h>
+#include <YComboBox.h>
+#include <YInputField.h>
 #include <YReplacePoint.h>
 #include <YUI.h>
 
@@ -52,6 +55,7 @@
 #define PROP_HEIGHT	12
 #define PROP_WIDTH	50
 
+void EditWidgetProperty(const std::string &name, YDialog *dialog, YWidget * widget);
 
 /**
  * Custom tree item class to map tree items to widgets
@@ -117,7 +121,7 @@ struct YDialogSpyPrivate
     YDialog *		spyDialog;	// Debug dialog that shows widget data
     YTree *		widgetTree;	// Tree widget to show widget hierarchy
     YPushButton * 	propButton;
-    YPushButton * 	addButton;
+    YMenuButton * 	addButton;
     YPushButton * 	deleteButton;
     YPushButton * 	upButton;
     YPushButton * 	downButton;
@@ -156,7 +160,7 @@ YDialogSpy::YDialogSpy( YDialog * targetDialog )
 
     YLayoutBox * hbox = fac->createHBox( vbox );
     priv->propButton = fac->createPushButton( hbox, "&Properties >>>" );
-    priv->addButton = fac->createPushButton( hbox, "&Add" );
+    priv->addButton = fac->createMenuButton( hbox, "&Add" );
     // FIXME: not implemented yet
     priv->addButton->setDisabled();
     priv->deleteButton = fac->createPushButton( hbox, "&Delete" );
@@ -201,7 +205,7 @@ void YDialogSpy::showProperties()
 	header->addColumn( "Type" );
 
 	priv->propTable = fac->createTable( minSize, header );
-	// priv->propTable->setKeepSorting( true );
+	priv->propTable->setNotify( true );
 
 	priv->propButton->setLabel( "<<< &Properties" );
 	priv->propReplacePoint->showChild();
@@ -303,68 +307,82 @@ void YDialogSpy::exec()
 
     while ( true )
     {
-	bool updateProp = false;
-	YEvent * event = priv->spyDialog->waitForEvent();
-	yuiMilestone() << "dialog: " << priv->spyDialog->preferredHeight();
-	yuiMilestone() << "tree: " << priv->widgetTree->preferredHeight();
+    	bool updateProp = false;
+    	YEvent * event = priv->spyDialog->waitForEvent();
+    	yuiMilestone() << "dialog: " << priv->spyDialog->preferredHeight();
+    	yuiMilestone() << "tree: " << priv->widgetTree->preferredHeight();
 
-	if ( event )
-	{
-        // window manager "close window" button
-	    if ( event->eventType() == YEvent::CancelEvent )
-	    {
-		priv->targetDialog->highlight( 0 );
-		return;
-	    }
-
-	    if ( event->widget() == priv->propButton )
-	    {
-		if ( propertiesShown() )
-		    hideProperties();
-		else
-		{
-		    showProperties();
-		    updateProp = true;
-		}
-	    }
-
-        YWidgetTreeItem * item = (YWidgetTreeItem *) priv->widgetTree->selectedItem();
-
-        if ( event->widget() == priv->deleteButton && item)
+        if ( event )
         {
-            YWidget *parent = item->widget()->parent();
-            YWidget *w = item->widget();
+            yuiMilestone() << "event: " << event;
 
-            if (w && parent)
+            // window manager "close window" button
+    	    if ( event->eventType() == YEvent::CancelEvent )
+    	    {
+        		priv->targetDialog->highlight( 0 );
+        		return;
+    	    }
+
+    	    if ( event->widget() == priv->propButton )
+    	    {
+        		if ( propertiesShown() )
+        		    hideProperties();
+        		else
+        		{
+        		    showProperties();
+        		    updateProp = true;
+        		}
+    	    }
+
+            YWidgetTreeItem * item = (YWidgetTreeItem *) priv->widgetTree->selectedItem();
+
+            if ( event->widget() == priv->deleteButton && item)
             {
-                yuiMilestone() << "removing widget: " << w;
-                parent->removeChild(w);
+                YWidget *parent = item->widget()->parent();
+                YWidget *w = item->widget();
 
-                if ( w->isValid() )
+                if (w && parent)
                 {
-                    delete w;
+                    yuiMilestone() << "removing widget: " << w;
+                    parent->removeChild(w);
+
+                    if ( w->isValid() )
+                    {
+                        delete w;
+                    }
+
+                    // redraw the target dialog
+                    priv->targetDialog->recalcLayout();
+
+                    // refresh the spy dialog
+                    priv->widgetTree->deleteAllItems();
+                    fillWidgetTree(priv->targetDialog, priv->widgetTree);
                 }
-
-                // redraw the target dialog
-                priv->targetDialog->recalcLayout();
-
-                // refresh the spy dialog
-                priv->widgetTree->deleteAllItems();
-                fillWidgetTree(priv->targetDialog, priv->widgetTree);
             }
+
+            if ( event->widget() == priv->propTable )
+            {
+                YTableItem *selected = dynamic_cast<YTableItem *>(priv->propTable->selectedItem());
+
+                if (selected)
+                {
+                    YTableCell *cell = selected->cell(0);
+                    yuiMilestone() << "editing property: " << cell->label();
+                    EditWidgetProperty(cell->label(), priv->targetDialog, item->widget());
+                }
+            }
+
+    	    if ( event->widget() == priv->widgetTree || updateProp )
+    	    {
+        		yuiDebug() << "Highlighting " << item << std::endl;
+
+        		if ( item )
+        		{
+        		    priv->targetDialog->highlight( item->widget() );
+        		    showProperties( item->widget() );
+        		}
+    	    }
         }
-
-	    if ( event->widget() == priv->widgetTree || updateProp )
-	    {
-		yuiDebug() << "Highlighting " << item << std::endl;
-
-		if ( item )
-		{
-		    priv->targetDialog->highlight( item->widget() );
-		    showProperties( item->widget() );
-		}
-	    }
-	}
     }
 }
 
@@ -380,4 +398,134 @@ void YDialogSpy::showDialogSpy( YDialog * dialog )
     {
 	YUI_CAUGHT( exception );
     }
+}
+
+void EditWidgetProperty(const std::string &name, YDialog *dialog, YWidget * widget)
+{
+    if (! widget) return;
+
+    YPropertyValue property = widget->getProperty( name );
+    yuiMilestone() << "editing property \"" << name << "\" (" << property.typeAsStr() << ")";
+    yuiMilestone().flush();
+
+    YPropertyType type = property.type();
+
+    // is the property read-only?
+    YPropertySet propSet = widget->propertySet();
+    for ( YPropertySet::const_iterator it = propSet.propertiesBegin();
+          it != propSet.propertiesEnd();
+          ++it )
+    {
+        YProperty prop = *it;
+
+        if (prop.name() == name)
+        {
+            if (prop.isReadOnly())
+            {
+                YDialog::showText("Property \"" + name + "\" is read only!");
+                return;
+            }
+            else
+            {
+                // finish the check
+                break;
+            }
+        }
+    }
+
+    if (type != YBoolProperty && type != YStringProperty && type != YIntegerProperty) return;
+
+    YPropertyValue orig = property;
+
+    YWidgetFactory *f = YUI::widgetFactory();
+
+    YDialog *popup = f->createPopupDialog();
+    YLayoutBox *vbox = f->createVBox(popup);
+
+    YComboBox *combo = NULL;
+    YInputField *input = NULL;
+
+    if (type == YBoolProperty)
+    {
+        combo = f->createComboBox( vbox, name);
+        combo->setNotify( true );
+
+        YItemCollection items;
+        items.push_back( new YItem( "true" ) );
+        items.push_back( new YItem( "false" ) );
+        combo->addItems( items );
+        combo->setValue( property.boolVal() ? "true" : "false" );
+    }
+    else
+    {
+        input = f->createInputField(vbox, name);
+        input->setNotify( true );
+
+        if (type == YStringProperty)
+        {
+            input->setValue( property.stringVal() );
+        }
+        // Integer property
+        else
+        {
+            input->setValue( std::to_string(property.integerVal()) );
+        }
+    }
+
+    YButtonBox * bbox = f->createButtonBox( vbox );
+    YPushButton *okButton = f->createPushButton( bbox, "OK" );
+    YPushButton *cancelButton = f->createPushButton( bbox, "Cancel" );
+
+    while ( true )
+    {
+        YEvent * event = popup->waitForEvent();
+        if ( event )
+        {
+            if (event->widget() == cancelButton || event->eventType() == YEvent::CancelEvent )
+            {
+                // restore the original value
+                widget->setProperty(name, orig);
+                break;
+            }
+            else if (event->widget() == okButton)
+            {
+                break;
+            }
+            else if (event->widget() == combo)
+            {
+                std::string value = combo->value();
+                yuiMilestone() << "Value changed to " << value;
+                widget->setProperty(name, YPropertyValue(value == "true"));
+                dialog->recalcLayout();
+            }
+            else if (event->widget() == input)
+            {
+                std::string value = input->value();
+                yuiMilestone() << "Value changed to " << value;
+
+                try {
+                    if (type == YIntegerProperty)
+                    {
+                        widget->setProperty(name, YPropertyValue(std::stoi(value)));
+                    }
+                    else
+                    {
+                        widget->setProperty(name, YPropertyValue(value));
+                    }
+
+                    dialog->recalcLayout();
+                }
+                catch(std::out_of_range)
+                {
+                    yuiWarning() << "Value '" << value << "' is out of integer range.";
+                }
+                catch(std::invalid_argument)
+                {
+                    yuiWarning() << "Value '" << value << "' is not a valid string.";
+                }
+            }
+        }
+    }
+
+    popup->destroy();
 }
