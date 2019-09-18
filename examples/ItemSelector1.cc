0c9b945d08ab6623fa66f6da1074fa51f64ec027
Stefan Hundhammer <shundhammer@suse.de>
2019-09-18 17:19:27 +0200

    Added config dialog for example

--- examples/ItemSelector1.cc
+++ examples/ItemSelector1.cc
@@ -14,11 +14,13 @@
 */
 
 
-// Simple ItemSelector example.
+// Comprehensive ItemSelector example.
 //
 // Compile with:
 //
 //     g++ -I/usr/include/yui -lyui ItemSelector1.cc -o ItemSelector1
+//
+// For a very simple ItemSelector example, see ItemSelector2-minimalistic.cc.
 
 #define YUILogComponent "example"
 #include "YUILog.h"
@@ -26,45 +28,54 @@
 #include "YUI.h"
 #include "YWidgetFactory.h"
 #include "YDialog.h"
-#include "YLayoutBox.h"
+#include "YAlignment.h"
+#include "YButtonBox.h"
+#include "YCheckBox.h"
+#include "YFrame.h"
+#include "YIntField.h"
 #include "YItemSelector.h"
 #include "YLabel.h"
+#include "YLayoutBox.h"
 #include "YPushButton.h"
-#include "YAlignment.h"
+#include "YRadioButton.h"
+#include "YRadioButtonGroup.h"
 #include "YEvent.h"
 
 
 using std::string;
 
-bool notify             = true;
-bool singleSelection    = false;
-bool selectorEnabled    = true;
-int  visibleItems       = 4;
+bool notify		= true;
+bool singleSelection	= false;
+int  visibleItems	= 4;
+bool selectorEnabled	= true;
 
 
 // Widgets
 
-YDialog         * dialog        = 0;
-YItemSelector	* selector      = 0;
-YLabel	        * valueField    = 0;
-YPushButton     * valueButton	= 0;
-YLabel	        * resultField	= 0;
-YPushButton     * resultButton	= 0;
-YPushButton     * focusButton	= 0;
-YPushButton     * enableButton	= 0;
-YPushButton     * closeButton	= 0;
+YDialog		* dialog	= 0;
+YItemSelector	* selector	= 0;
+YLabel		* valueField	= 0;
+YPushButton	* valueButton	= 0;
+YLabel		* resultField	= 0;
+YPushButton	* resultButton	= 0;
+YPushButton	* focusButton	= 0;
+YPushButton	* enableButton	= 0;
+YPushButton	* closeButton	= 0;
 
 
 void createWidgets();
 void handleEvents();
 void updateValueField();
 void updateResultField();
+void askConfigValues();
 
 
 int main( int argc, char **argv )
 {
     YUILog::setLogFileName( "/tmp/libyui-examples.log" );
     YUILog::enableDebugLogging();
+
+    askConfigValues();
 
     createWidgets();
     handleEvents();
@@ -73,16 +84,23 @@ int main( int argc, char **argv )
 }
 
 
+/**
+ * Create the widget tree for the main dialog
+ **/
 void createWidgets()
 {
-    dialog	 = YUI::widgetFactory()->createPopupDialog();
-    YAlignment * mbox	 = YUI::widgetFactory()->createMarginBox( dialog, 2, 2, 0.4, 0.4 );
-    YLayoutBox * vbox	 = YUI::widgetFactory()->createVBox( mbox );
+    yuiMilestone() << endl;
 
-    YUI::widgetFactory()->createHeading( vbox, "Pizza Menu" );
-    YUI::widgetFactory()->createVSpacing( vbox, 0.2 );
+    YWidgetFactory * fac = YUI::widgetFactory();
 
-    selector = YUI::widgetFactory()->createItemSelector( vbox, singleSelection );
+    dialog		= fac->createPopupDialog();
+    YAlignment * mbox	= fac->createMarginBox( dialog, 2, 2, 0.4, 0.4 );
+    YLayoutBox * vbox	= fac->createVBox( mbox );
+
+    fac->createHeading( vbox, "Pizza Menu" );
+    fac->createVSpacing( vbox, 0.2 );
+
+    selector = fac->createItemSelector( vbox, singleSelection );
     YUI_CHECK_PTR( selector );
 
     YItemCollection items;
@@ -97,54 +115,57 @@ void createWidgets()
     selector->setVisibleItems( visibleItems );
 
     if ( notify )
-        selector->setNotify();
+	selector->setNotify();
 
-    YUI::widgetFactory()->createVSpacing( vbox, 0.4 );
+    fac->createVSpacing( vbox, 0.4 );
 
-    YLayoutBox * hbox1	= YUI::widgetFactory()->createHBox( vbox );
-    valueField = YUI::widgetFactory()->createOutputField( hbox1, "<unknown>" );
+    YLayoutBox * hbox1	= fac->createHBox( vbox );
+    valueField		= fac->createOutputField( hbox1, "<unknown>" );
     valueField->setStretchable( YD_HORIZ, true ); // allow stretching over entire dialog width
 
-    valueButton = YUI::widgetFactory()->createPushButton( hbox1, "&Value" );
+    valueButton		= fac->createPushButton( hbox1, "&Value" );
 
-    YUI::widgetFactory()->createVSpacing( vbox, 0.3 );
+    fac->createVSpacing( vbox, 0.3 );
 
-    YLayoutBox * hbox2	 = YUI::widgetFactory()->createHBox( vbox );
-    resultField = YUI::widgetFactory()->createOutputField( hbox2, "<selected items>\n\n\n\n\n" );
+    YLayoutBox * hbox2	= fac->createHBox( vbox );
+    resultField		= fac->createOutputField( hbox2, "<selected items>\n\n\n\n\n" );
     resultField->setStretchable( YD_HORIZ, true ); // allow stretching over entire dialog width
 
-    resultButton = YUI::widgetFactory()->createPushButton( hbox2, "&Selected\nItems" );
+    resultButton		= fac->createPushButton( hbox2, "&Selected\nItems" );
 
-    YUI::widgetFactory()->createVSpacing( vbox, 0.3 );
+    fac->createVSpacing( vbox, 0.3 );
 
-    YAlignment	* alignment1	  = YUI::widgetFactory()->createLeft( vbox );
-    YLayoutBox	* buttonBox	  = YUI::widgetFactory()->createHBox( alignment1 );
-    focusButton	  = YUI::widgetFactory()->createPushButton( buttonBox, "Set &Focus" );
-    enableButton	  = YUI::widgetFactory()->createPushButton( buttonBox, "&Enable"    );
+    YAlignment	* alignment1	= fac->createLeft( vbox );
+    YLayoutBox	* buttonBox	= fac->createHBox( alignment1 );
+    focusButton			= fac->createPushButton( buttonBox, "Set &Focus" );
+    enableButton		= fac->createPushButton( buttonBox, "&Enable"	 );
 
-    YAlignment	* rightAlignment  = YUI::widgetFactory()->createRight( vbox );
-    closeButton	  = YUI::widgetFactory()->createPushButton( rightAlignment, "&Close" );
+    YAlignment * rightAlignment = fac->createRight( vbox );
+    closeButton			= fac->createPushButton( rightAlignment, "&Close" );
 }
 
 
+/**
+ * Event loop and event handling for the main dialog
+ **/
 void handleEvents()
 {
+    yuiMilestone() << endl;
+
     while ( true )
     {
 	YEvent * event = dialog->waitForEvent();
 
 	if ( event )
 	{
-	    if ( event->eventType() == YEvent::CancelEvent ) // window manager "close window" button
+	    if ( event->widget()    == closeButton ||
+		 event->eventType() == YEvent::CancelEvent ) // window manager "close window" button
+	    {
 		break; // leave event loop
-
-	    if ( event->widget() == closeButton )
-		break; // leave event loop
+	    }
 
 	    if ( event->widget() == focusButton )
-	    {
 		selector->setKeyboardFocus();
-	    }
 
 	    if ( event->widget() == enableButton )
 	    {
@@ -176,9 +197,9 @@ void updateValueField()
     YItem * item = selector->selectedItem();
 
     if ( item )
-        valueField->setValue( item->label() );
+	valueField->setValue( item->label() );
     else
-        valueField->setValue( "<none>" );
+	valueField->setValue( "<none>" );
 }
 
 
@@ -188,11 +209,95 @@ void updateResultField()
 
     for ( YItem * item: selector->selectedItems() )
     {
-        if ( ! result.empty() )
-            result += "\n";
+	if ( ! result.empty() )
+	    result += "\n";
 
-        result += item->label();
+	result += item->label();
     }
 
     resultField->setText( result );
+}
+
+
+/**
+ * Ask the user some configuration values for the main dialog.
+ * All values are stored in global variables.
+ *
+ *    +---------------------------+
+ *    |   Program Configuration   |
+ *    |                           |
+ *    |  Selection Mode           |
+ *    |  (x) Single Selection     |
+ *    |  ( ) Multi-Selection      |
+ *    |                           |
+ *    |  [x] Notify               |
+ *    |                           |
+ *    |  Visible Items:           |
+ *    |  [ 4          ]           |
+ *    |                           |
+ *    |   [Continue]  [Cancel]    |
+ *    +---------------------------+
+ **/
+void askConfigValues()
+{
+    yuiMilestone() << endl;
+
+    YWidgetFactory * fac = YUI::widgetFactory();
+
+    YDialog    * dialog = fac->createPopupDialog();
+    YAlignment * mbox	= fac->createMarginBox( dialog, 2, 2, 0.4, 0.4 );
+    YLayoutBox * vbox	= fac->createVBox( mbox );
+
+    fac->createHeading( vbox, "Program Configuration" );
+    fac->createVSpacing( vbox, 0.2 );
+
+    YAlignment		* left		   = fac->createLeft( vbox );
+    YRadioButtonGroup	* radioButtonGroup = fac->createRadioButtonGroup( left );
+    YFrame		* frame		   = fac->createFrame( radioButtonGroup, "Selection Mode" );
+    YLayoutBox		* rbVBox	   = fac->createVBox( frame );
+
+    left = fac->createLeft( rbVBox );
+    YRadioButton * singleSelectionButton = fac->createRadioButton( left, "&Single Selection",  singleSelection );
+    left = fac->createLeft( rbVBox );
+    YRadioButton * multiSelectionButton	 = fac->createRadioButton( left, "&Multi-Selection", ! singleSelection );
+    (void) multiSelectionButton;
+
+    fac->createVSpacing( vbox, 0.6 );
+
+    left = fac->createLeft( vbox );
+    YCheckBox * notifyCheckBox = fac->createCheckBox( left, "&Notify", notify );
+
+    fac->createVSpacing( vbox, 0.6 );
+    left = fac->createLeft( vbox );
+    YIntField * visibleItemsField = fac->createIntField( left, "&Visible Items:", 1, 10, visibleItems );
+
+    fac->createVSpacing( vbox, 1 );
+
+    YButtonBox	* buttonBox	 = fac->createButtonBox( vbox );
+    YPushButton * continueButton = fac->createPushButton( buttonBox, "C&ontinue" );
+    YPushButton * cancelButton	 = fac->createPushButton( buttonBox, "&Cancel" );
+
+    continueButton->setRole( YOKButton );
+    cancelButton->setRole( YCancelButton );
+
+    YEvent * event = dialog->waitForEvent();
+
+    if ( event->widget() == continueButton )
+    {
+	notify		= notifyCheckBox->value();
+	singleSelection = singleSelectionButton->value();
+	visibleItems	= visibleItemsField->value();
+
+	yuiMilestone() << "Notify: " << notify << endl;
+	yuiMilestone() << "Single selection: " << singleSelection << endl;
+	yuiMilestone() << "Visible items: " << visibleItems << endl;
+    }
+    else if ( event->widget() == cancelButton ||
+	      event->eventType() == YEvent::CancelEvent ) // window manager "close window" button
+    {
+        dialog->destroy();
+	exit( 1 );
+    }
+
+    dialog->destroy();
 }
